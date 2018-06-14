1844631788684dbee65e0d6aa7f9128e2f991ca5
johny <jasiek.krol.pl@gmail.com>
2018-06-14 18:39:52 +0200

    template

---
+++ application/modules/templates/controllers/Templates.php
@@ -0,0 +1,26 @@
+<?php 
+	class Templates extends MX_Controller
+	{
+		function __construct()
+		{
+			parent::__construct();
+		}
+		function test()
+		{
+			$data = "";
+			$this->public_bootstrap($data);
+		}
+		function public_bootstrap($data)
+		{
+			$this->load->view('public_bootstrap', $data);
+		}
+		function public_jqm($data)
+		{
+			$this->load->view('public_jqm', $data);
+		}
+		function admin($data)
+		{
+			$this->load->view('admin', $data);
+		}
+	}
+?>
